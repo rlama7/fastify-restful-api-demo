47cf7e481136ddfa9dfa90be7b9fa93894227800
Ratna Lama <royalray7@gmail.com>
2022-03-31 17:56:51 -0700

    created mock data

---
+++ Items.js
@@ -0,0 +1,14 @@
+let items = [
+  { id: '1', name: 'Ray Love', occupation: 'Engineer', company: 'iTouch' },
+  { id: '2', name: 'Maya Miola', occupation: 'Driver', company: 'uTouch' },
+  {
+    id: '3',
+    name: 'Jennifer Junita',
+    occupation: 'Pilot',
+    company: 'iPlane',
+  },
+  { id: '4', name: 'Sofia Santana', occupation: 'Diver', company: 'iDrive' },
+  { id: '5', name: 'Lily Lopez', occupation: 'Investor', company: 'iFX' },
+  { id: '6', name: 'Sam SantaCruz', occupation: 'Actor', company: 'iMedia' },
+  { id: '7', name: 'Billy Biggie', occupation: 'Singer', company: 'iRedLable' },
+];
